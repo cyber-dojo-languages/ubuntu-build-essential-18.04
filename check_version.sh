2fdacb16184acadfce30fbd2aa6924f64ef0dcf4
Jon Jagger <jon@jaggersoft.com>
2018-02-10 09:00:23 +0000

    fixed broken version-check

--- check_version.sh
+++ check_version.sh
@@ -2,7 +2,7 @@
 set -e
 
 readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
-readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/ubuntu-build-essential:18.04 sh -c 'cat /etc/issue')
+readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/ubuntu-build-essential:18.04 sh -c 'cat /etc/os-release')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
