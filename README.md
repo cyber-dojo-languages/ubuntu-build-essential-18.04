5218f107784cacd59e0355ab5af7cf824386df4a
Jon Jagger <jon@jaggersoft.com>
2018-02-28 15:48:46 +0000

    readme version now hyperlinks to version-check script

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 ![Build Status](https://travis-ci.org/cyber-dojo-languages/ubuntu-build-essential-18.04.svg?branch=master)
 
-Version=18.04
+[Version=18.04](https://github.com/cyber-dojo-languages/ubuntu-build-essential-18.04/blob/master/check_version.sh)
--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
+readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly IMAGE_NAME=cyberdojofoundation/ubuntu-build-essential:18.04
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c '. /etc/os-release && echo ${VERSION_ID}')
 
